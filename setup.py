a634bea709fd9a957fe1d8c3e89cc2dc974928a6
Daniil Nababkin <43863412+TheRavehorn@users.noreply.github.com>
2020-08-23 01:06:18 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
+import subprocess
+import time
+
 try:
     import scapy
 except ModuleNotFoundError:
     print("Installing missing dependencies...")
     time.sleep(3)
     subprocess.call("apt-get update", shell=True)
+    subprocess.call("apt-get install python3-pip -y", shell=True)
     subprocess.call("pip3 install scapy --quiet", shell=True)
 finally:
     subprocess.call("clear", shell=True)
